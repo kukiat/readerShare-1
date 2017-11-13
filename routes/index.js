64241fc12e901452783409473f5e0bc4f0adffb5
kukiat <kukiat1996@gmail.com>
2017-11-13 15:44:42 +0700

    jwt with mysql

--- routes/index.js
+++ routes/index.js
@@ -11,39 +11,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/feeds', function(req, response,next) {
-  client.connect()
-  const text = 'INSERT INTO test(text, complete) VALUES($1, $2) RETURNING *'
-  const values = ['brianc', true]
-  console.log(req.body.xxx)
-  client.query(text, values)
-    .then(res => {
-      response.status(200).send({
-        message: 'create success',
-      })
-    })
-    .catch(err => console.error(err.stack))
+router.get('/feeds', function(req, res,next) {
+  res.json(res)
 })
 
-router.get('/todo/:id', function(req, response,next) {
-  client.connect()
-  client.query(`
-    SELECT * FROM test WHERE id=${req.params.id}
-  `)
-    .then(res => response.json(res.rows))
-    .catch(err => console.error(err.stack))
+router.get('/review/:id', function(req, res,next) {
+  res.json()
 })
 
-router.get('/todo', function(req, response,next) {
-  client.connect()
-  const text = 'SELECT * FROM test'
-
-  client.query(text)
-    .then(res => response.json(res.rows))
-    .catch(err => console.error(err.stack))
+router.post('/post', function(req, res,next) {
+  res.json()
 })
 
-router.post('/login', function(req, response, next) {
+router.post('/comment', function(req, res,next) {
+  res.json()
+})
+
+router.post('/subscribe', function(req, res,next) {
+  res.json()
+})
+
+router.post('/login', function(req, res, next) {
   //authen 
   const email = req.body.email
   const password = req.body.password
@@ -51,24 +39,24 @@ router.post('/login', function(req, response, next) {
   const text = 'SELECT * FROM users WHERE email=$1'
   const values = [email]
   client.query(text, values)
-    .then(res => {
-      const user = res.rows[0]
+    .then(users => {
+      const user = users.rows[0]
       if(!user){
-        response.status(401).end('wrong email')
+        res.status(401).end('wrong email')
       }else if(user) {
         bcrypt.compare(password, user.password, (err, isMatch) => {
           if(err)
-            response.json(err)
+            res.json(err)
           else if(!isMatch) {
-            response.status(401).send('worng password')
+            res.status(401).send('worng password')
           }else {
             console.log(user.email)
             console.log(user.name)
             const token = jwt.sign({
               email: user.email, 
               name: user.name
-            }, 'jwtforadv',{ expiresIn: 1 * 30 })
-            response.status(200).send(token)
+            }, 'jwtforadv',{ expiresIn: 60 * 1 })
+            res.status(200).send(token)
           }
         })
           
@@ -89,7 +77,7 @@ router.post('/login', function(req, response, next) {
     .catch(err => console.error(err.stack))
 })
 // CREATE TABLE user(id SERIAL PRIMARY KEY, email VARCHAR(40) not null, password VARCHAR(100) not null ,name VARCHAR(40) not null);
-router.post('/register', function(req, response, next) {
+router.post('/register', function(req, res, next) {
   //need to validate
   const name = req.body.name
   const email = req.body.email
@@ -101,25 +89,25 @@ router.post('/register', function(req, response, next) {
   const values1 = [email]
   
   client.query(text1, values1)
-    .then(res => {
-      const user = res.rows[0]
+    .then(users => {
+      const user = users.rows[0]
       if(!user){
         client.query(text, values)
-        .then(res => response.status(200).send('register success'))
+        .then(users => res.status(200).send('register success'))
         .catch(err => console.error(err.stack))
       }else if(user){
-        response.status(422).send('already use email')
+        res.status(422).send('already use email')
 
       }
     })
 })
 
-router.get('/users', function(req, response, next) {
+router.get('/users', function(req, res, next) {
   client.connect()
   const text = 'SELECT * FROM users'
 
   client.query(text)
-    .then(res => response.json(res.rows))
+    .then(res => res.json(res.rows))
     .catch(err => console.error(err.stack))
 })
 

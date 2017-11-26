0e86a7bad779e643524051d3e442adf49c0f7d05
kukiat <kukiat1996@gmail.com>
2017-11-27 01:17:48 +0700

    change toure to /mock/<route>

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/feeds', function(req, res, next) {
+router.get('/mock/feeds', function(req, res, next) {
   let feed = require('../utils/feed')
   res.json(feed.getFeed())
 })
 
-router.get('/review/:id', function(req, res, next) {
+router.get('/mock/review/:id', function(req, res, next) {
   let getFeed = require('../utils/getFeed')
   res.json(getFeed.findFeed(req.params.id))
 })
 
-router.post('/subscribe', function(req, res, next) {
+router.post('/mock/subscribe', function(req, res, next) {
   Model.notification()
   res.send('ok')
 })

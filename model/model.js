ef95272a6c501a3cef65a86cc65f63a130344daf
kukiat <kukiat1996@gmail.com>
2017-11-27 01:29:42 +0700

    delete require firebase admin

--- model/model.js
+++ model/model.js
@@ -1,12 +1,5 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("../utils/readershare-1-firebase-adminsdk-vjyk4-83d561f008.json");
 var MicroGear = require('microgear')
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://readershare-1.firebaseio.com"
-});
-var db = admin.database();
 
 var microgear = MicroGear.create({
   key : "6xeLdlHHWBuM49O",

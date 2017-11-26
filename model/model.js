947934181b26e14924f4690128fec1bdff974996
kukiat <kukiat1996@gmail.com>
2017-11-27 01:12:57 +0700

    delete trash route and login/regis

--- model/model.js
+++ model/model.js
@@ -15,27 +15,6 @@ var microgear = MicroGear.create({
 });
 
 module.exports = {
-	testPush : function(object){
-		db.ref("posts").child("post").set(object);
-	},
-	testRetrieve : function(){
-		db.ref("posts").on("value", function(snapshot) {
-		  console.log(snapshot.val());
-		}, function (errorObject) {
-		  console.log("The read failed: " + errorObject.code);
-		})
-	},
-	register : async function(email, password_hashed){
-		return await new Promise((resolve, reject)=>{
-			isNotHasUser(email).then(()=>{
-				admin.auth().createUser({email, password_hashed})
-				.then(function(userRecord) {resolve('success')})
-				.catch(function(error) {reject('failed')});
-			}).catch(()=>{
-				reject('This email is exist');
-			})
-		})
-	},
 	notification: function() {
 		microgear.connect('noti')
 		microgear.on('connected', function() {
@@ -45,15 +24,3 @@ module.exports = {
 		})
 	}
 }
-
-async function isNotHasUser(email){
-	return await new Promise(function(resolve, reject){
-		admin.auth().getUserByEmail(email)
-		  .then(function(userRecord) {
-		    reject()
-		  }).catch(function(error) {
-		    resolve()
-		  });
-	})
-}
-
--- routes/index.js
+++ routes/index.js
@@ -19,72 +19,9 @@ router.get('/review/:id', function(req, res, next) {
   res.json(getFeed.findFeed(req.params.id))
 })
 
-router.post('/post', function(req, res, next) {
-  res.json()
-})
-
-router.post('/comment', function(req, res, next) {
-  res.json()
-})
-
 router.post('/subscribe', function(req, res, next) {
   Model.notification()
   res.send('ok')
-})
-
-router.post('/testpush',function(req, res, next){
-  Model.testPush(req.body);
-  res.send('testPush')
-})
-router.get('/testretrieve',function(req, res, next){
-  Model.testRetrieve();
-  res.send('testRetrieve')
-})
-
-router.post('/login', function(req, res, next) {
-  //authen 
-  const email = req.body.email
-  const password = req.body.password
-
-  if(!user){
-    res.status(401).end('wrong email')
-  }else if(user) {
-    bcrypt.compare(password, user.password, (err, isMatch) => {
-    if(err)
-      res.json(err)
-    else if(!isMatch) {
-      res.status(401).send('worng password')
-    }else {
-      const token = jwt.sign({
-        email: user.email, 
-        name: user.name
-      }, 'jwtforadv',{ expiresIn: 60 * 1 })
-        res.status(200).send(token)
-      }
-    })
-  }
-})
-router.post('/register', function(req, res, next) {
-  const name = req.body.name
-  const email = req.body.email
-  const password_hashed = bcrypt.hashSync(req.body.password, 10)
-
-  Model.register(email, password_hashed).then((data)=>{
-    res.send(data)
-  }).catch((data)=>{
-    res.send(data)
-  })
-})
-
-router.post('/whoami', function(req, res, next) {
-  const token = req.body.token
-  var decoded = jwt.verify(token, 'jwtforadv', (err, decoded) => {
-    if(err)
-      res.json(err)
-    else
-      res.json(decoded)
-  })
-  
 })
 
 module.exports = router;

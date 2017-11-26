5d5006bf0bda3bfde9d0f667b6c32fef5b4d603e
kukiat <kukiat1996@gmail.com>
2017-11-27 00:51:20 +0700

    test delete all routing

--- routes/index.js
+++ routes/index.js
@@ -25,30 +25,7 @@ router.get('/feeds', function(req, res, next) {
   .catch((err)=>res.send(err))
 })
 
-router.get('/review/:reviewId', function(req, res, next) {
-  Model.getReview(req.params.reviewId)
-  .then(data => res.status(200).send(data))
-  .catch(data => res.send(data))
-})
 
-router.post('/post', function(req, res, next) {
-  Model.postReview({
-    uId: req.body.uId, 
-    bookName: req.body.bookName, 
-    reviewTitle: req.body.reviewTitle, 
-    reviewContent: req.body.reviewContent
-  })
-    .then(data => {
-      res.status(200).send('ok')
-    })
-    .catch(data => res.status(400).send(data))
-})
-
-router.post('/subscribe', function(req, res, next) {
-  Model.subscribe(req.body.subscriber, req.body.follower)
-    .then(data => res.status(200).send(data))
-    .catch(data => res.status(400).send(data))
-})
 module.exports = router;
 
 

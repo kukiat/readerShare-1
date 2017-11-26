b30c79f0b6f228cb4f62937cc2c8d1cbfa543b74
kukiat <kukiat1996@gmail.com>
2017-11-27 00:56:23 +0700

    fixbug2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
-var jwt = require('jsonwebtoken')
-var bcrypt = require('bcrypt')
 var router = express.Router();
 var Model = require('../model/model');
-var MicroGear = require('microgear')
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -25,7 +22,30 @@ router.get('/feeds', function(req, res, next) {
   .catch((err)=>res.send(err))
 })
 
+router.get('/review/:reviewId', function(req, res, next) {
+  Model.getReview(req.params.reviewId)
+  .then(data => res.status(200).send(data))
+  .catch(data => res.send(data))
+})
 
+router.post('/post', function(req, res, next) {
+  Model.postReview({
+    uId: req.body.uId, 
+    bookName: req.body.bookName, 
+    reviewTitle: req.body.reviewTitle, 
+    reviewContent: req.body.reviewContent
+  })
+    .then(data => {
+      res.status(200).send('ok')
+    })
+    .catch(data => res.status(400).send(data))
+})
+
+router.post('/subscribe', function(req, res, next) {
+  Model.subscribe(req.body.subscriber, req.body.follower)
+    .then(data => res.status(200).send(data))
+    .catch(data => res.status(400).send(data))
+})
 module.exports = router;
 
 

0803e50d33949456afff46a4979c7f992617a1cd
kukiat <kukiat1996@gmail.com>
2017-11-27 01:33:11 +0700

    add require firebase

--- model/model.js
+++ model/model.js
@@ -1,5 +1,13 @@
 var admin = require("firebase-admin");
 var MicroGear = require('microgear')
+var firebase = require('firebase')
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCxV1SPVPdDQg539Ir6G0Rn5Y-WgAAgzVs",
+  authDomain: "reader-db.firebaseapp.com",
+  databaseURL: "https://reader-db.firebaseio.com",
+  storageBucket: "reader-db.appspot.com"
+})
 
 var microgear = MicroGear.create({
   key : "6xeLdlHHWBuM49O",
@@ -7,13 +15,15 @@ var microgear = MicroGear.create({
   alias: 'server'
 });
 
+var database = firebase.database();
+
 module.exports = {
-	notification: function() {
-		microgear.connect('noti')
-		microgear.on('connected', function() {
-			console.log('test')
-			microgear.publish('/message','eieieeiieieie.')
-			microgear.disconnect()
-		})
-	}
+  notification: function() {
+  	microgear.connect('noti')
+      microgear.on('connected', function() {
+        console.log('test')
+		  microgear.publish('/message','eieieeiieieie.')
+		    microgear.disconnect()
+	  })
+  }
 }

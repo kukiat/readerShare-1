424b477e4b62f6a9ccaf0403deb5b4ea0ac30a16
kukiat <kukiat1996@gmail.com>
2017-12-02 05:08:50 +0700

    change response-file to arrow-func

--- utils/response.js
+++ utils/response.js
@@ -1,13 +1,15 @@
 module.exports = {
-  respondResult: (res) => (err) => {
-    res.status(err.status).send(err)
-  },
-  respondError: (res) => () => {
-    res.status(200).send({message: 'success'})
-  },
-  respondSuccess: (res) => (result) => {
+  respondResult: (res) => (result) => {
     res.status(200).send({
       message: 'success',
       result
     })  
+  },
+  respondError: (res) => (err) => {
+    res.status(err.status).send(err)
+  }
+  ,
+  respondSuccess: (res) => () => {
+    res.status(200).send({message: 'success'})
+  }
 }
